db4cbe042714a956df9322e2126a776c6e342176
AJ Green <ajgreen1987@gmail.com>
2013-10-08 00:58:07 -0400

    - MIT license

--- GMSMessageWindow.m
+++ GMSMessageWindow.m
@@ -1,10 +1,25 @@
 //
-//  GMSMessageWindow.m
-//  AirplaneBomb
+//GMSMessageWindow.m
+//The MIT License (MIT)
+//Copyright (c) 2013 AJ Green
 //
-//  Created by AJ Green on 10/7/13.
-//  Copyright (c) 2013 Green Mailbox Studios. All rights reserved.
+//Permission is hereby granted, free of charge, to any person obtaining a copy of
+//this software and associated documentation files (the "Software"), to deal in
+//the Software without restriction, including without limitation the rights to
+//use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+//the Software, and to permit persons to whom the Software is furnished to do so,
+//subject to the following conditions:
 //
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+//FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+//COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+//IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 
 #import "GMSMessageWindow.h"
 
@@ -27,7 +42,7 @@
     label.fontSize = fontSize;
     label.fontColor = fontColor;
     
-    SKAction *drawCharacters = [SKAction customActionWithDuration:1.5f actionBlock:^(SKNode *node, CGFloat elapsedTime) {
+    SKAction *drawCharacters = [SKAction customActionWithDuration:duration actionBlock:^(SKNode *node, CGFloat elapsedTime) {
         SKLabelNode *label = (SKLabelNode*)node;
         
         if (counter < numberOfCharacters)
@@ -40,6 +55,16 @@
     [label runAction:drawCharacters];
     
     [toReturn addChild:label];
+    
+    SKLabelNode *next = [SKLabelNode labelNodeWithFontNamed:font];
+    
+    next.text = @"Tap to continue...";
+    next.fontSize = fontSize;
+    next.fontColor = fontColor;
+    next.zPosition = 1;
+    next.position = CGPointMake(CGRectGetMaxX(toReturn.frame)-((next.frame.size.width*0.5f)+10), CGRectGetMinY(toReturn.frame)+4.0f);
+    
+    [toReturn addChild:next];
     
     return toReturn;
 }
@@ -66,7 +91,7 @@
     label.fontSize = fontSize;
     label.fontColor = fontColor;
     
-    SKAction *drawCharacters = [SKAction customActionWithDuration:1.5f actionBlock:^(SKNode *node, CGFloat elapsedTime) {
+    SKAction *drawCharacters = [SKAction customActionWithDuration:duration actionBlock:^(SKNode *node, CGFloat elapsedTime) {
         SKLabelNode *label = (SKLabelNode*)node;
         
         if (counter < numberOfCharacters)
